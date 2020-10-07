6dd4138b507d51a9a4352edcdda72929e6b2e207
Siddhant <sjsid9@gmail.com>
2020-10-07 23:54:52 +0530

    contenthash and cleanWebpack Plugin and prod mode configs
    Signed-off-by: Siddhant <sjsid9@gmail.com>

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,16 +2,14 @@ const path = require('path');
 const cleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/app.js',
   output: {
+    // filename: '[contenthash].js',
     filename: 'app.js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
     publicPath: 'assets/scripts/',
   },
-  // devtool: 'cheap-module-eval-source-map',
-  plugins: [new cleanPlugin.CleanWebpackPlugin()],
-  // devServer: {
-  //   contentBase: './',
-  // },
+  devtool:'cheap-source-map',
+  plugins: [new cleanPlugin.CleanWebpackPlugin()]
 };
